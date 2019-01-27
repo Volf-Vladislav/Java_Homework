ba96c666961687aacdfe63c65efc79f2edeffbfb
Volf-Vladislav <43464334+Volf-Vladislav@users.noreply.github.com>
2019-01-27 18:10:56 +0300

    Update Task_8.java

--- Task_8.java
+++ Task_8.java
@@ -3,12 +3,12 @@ public class Task_8 {
     
 	public static void main(String[] lol) {
 		int a = new Scanner(System.in).nextInt();
-        int sum = 0;
+        	int sum = 0;
         
-        while (a > 0) {
-            sum = sum + a % 10;
-            a = a / 10;
-        }
-        System.out.println(sum);
+        	while (a > 0) {
+            		sum = sum + a % 10;
+            		a = a / 10;
+        	}
+        	System.out.println(sum);
 	}
 }
